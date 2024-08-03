4fee0167d26b0a7a82076ad3a3739fd08a91ac0b
BubbleFish <daforsastudia@gmail.com>
2024-08-03 20:47:31 +0300

    refactor: Move Letter parser to use the new parser module

--- protocol/letter_parser.go
+++ protocol/letter_parser.go
@@ -2,72 +2,38 @@ package protocol
 
 import (
 	"bufio"
-	"encoding/binary"
-	"io"
 
 	"github.com/bbfh-tuxle/lib/field"
+	"github.com/bbfh-tuxle/lib/field/parser"
 )
-
-func readString(reader *bufio.Reader, delimiter byte) (string, error) {
-	data, err := reader.ReadString(delimiter)
-	if err != nil {
-		return "", err
-	}
-
-	return data[:len(data)-1], nil
-}
-
-func readUInt32(reader *bufio.Reader, delimiter byte) (uint32, error) {
-	data, err := reader.ReadBytes(delimiter)
-	if err != nil {
-		return 0, err
-	}
-
-	return binary.BigEndian.Uint32(data[:len(data)-1]), nil
-}
 
 // Reads (parses) Letter.Type (and only that).
 //
 // Use to avoid parsing the entire letter when only type is required (e.g. routing).
-func ReadLetterType(stream io.Reader) (string, error) {
-	return readString(bufio.NewReader(stream), ' ')
+func ReadLetterType(reader *bufio.Reader) (string, error) {
+	return parser.ReadString(reader, ' ')
 }
 
 // Reads (parses) the entire Letter.
-func ReadLetter(stream io.Reader) (Letter, error) {
-	reader := bufio.NewReader(stream)
-
-	letterType, err := readString(reader, ' ')
+func ReadLetter(reader *bufio.Reader) (Letter, error) {
+	letterType, err := parser.ReadString(reader, ' ')
 	if err != nil {
 		return Letter{}, err
 	}
 
-	letterEndpoint, err := readString(reader, '\n')
+	letterEndpoint, err := parser.ReadString(reader, '\n')
 	if err != nil {
 		return Letter{}, err
 	}
 
-	numberOfParameters, err := readUInt32(reader, '\n')
+	numberOfParameters, err := parser.ReadUInt32(reader, '\n')
 	if err != nil {
 		return Letter{}, err
 	}
 
-	var parameters = field.NewParameters()
-	for range numberOfParameters {
-		key, err := readString(reader, '=')
-		if err != nil {
-			return Letter{}, err
-		}
+	parameters, err := field.ReadParameters(reader, int(numberOfParameters))
 
-		value, err := readString(reader, '\n')
-		if err != nil {
-			return Letter{}, err
-		}
-
-		parameters[key] = value
-	}
-
-	body, err := readString(reader, '\r')
+	body, err := parser.ReadString(reader, '\r')
 	if err != nil {
 		return Letter{}, err
 	}
