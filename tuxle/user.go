10c6cde5e360ca669362a357be79e0c56d2ddfb9
BubbleFish <daforsastudia@gmail.com>
2024-08-05 19:33:13 +0300

    feat: Add User type

---
+++ tuxle/user.go
@@ -0,0 +1,26 @@
+package tuxle
+
+import (
+	"github.com/bbfh-tuxle/lib/internal/escape"
+	"github.com/bbfh-tuxle/lib/internal/stream"
+	"github.com/bbfh-tuxle/lib/tuxle/fields"
+)
+
+type User struct {
+	Id          string
+	Name        string
+	PictureURI  string
+	Description string
+	Password    fields.Password
+	Permissions fields.Permissions
+}
+
+func (user User) String() string {
+	return stream.WriteToString(fields.Parameters{
+		"Name":        user.Name,
+		"PictureURI":  user.PictureURI,
+		"Description": escape.EscapeString(user.Description),
+		"Password":    user.Password.String(),
+		"Permissions": user.Permissions.String(),
+	})
+}
---
+++ tuxle/user_parser.go
@@ -0,0 +1,47 @@
+package tuxle
+
+import (
+	"bufio"
+
+	"github.com/bbfh-tuxle/lib/internal/escape"
+	"github.com/bbfh-tuxle/lib/internal/stream"
+	"github.com/bbfh-tuxle/lib/tuxle/channels"
+	"github.com/bbfh-tuxle/lib/tuxle/fields"
+)
+
+func ReadUser(id string, file channels.File) (User, error) {
+	parameters, err := fields.ReadAllParameters(bufio.NewReader(file))
+	if err != nil {
+		return User{}, err
+	}
+
+	errs := parameters.Validate(map[string]fields.ValidateFunc{
+		"Name":        fields.Exists,
+		"PictureURI":  fields.Exists,
+		"Description": fields.Exists,
+		"Password":    fields.Exists,
+		"Permissions": fields.Exists,
+	})
+	if len(errs) != 0 {
+		return User{}, stream.CombineErrors(errs...)
+	}
+
+	password, err := fields.ParsePassword(parameters["Password"])
+	if err != nil {
+		return User{}, err
+	}
+
+	permissions, err := fields.ParsePermissions(fields.CAN_READ, parameters["Password"])
+	if err != nil {
+		return User{}, err
+	}
+
+	return User{
+		Id:          id,
+		Name:        parameters["Name"],
+		PictureURI:  parameters["PictureURI"],
+		Description: escape.UnescapeString(parameters["Description"]),
+		Password:    password,
+		Permissions: permissions,
+	}, nil
+}
