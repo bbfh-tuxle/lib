eb236f7091b2f6f7d1be724fad6ed1748996f460
BubbleFish <daforsastudia@gmail.com>
2024-08-05 14:51:40 +0300

    docs: Fix file documentation

--- tuxle/channels/file.go
+++ tuxle/channels/file.go
@@ -1,6 +1,6 @@
 package channels
 
-// A file that message list will read from.
+// A file that channels will read/write from/to.
 //
 // An *os.File will always satisfy this interface.
 type File interface {
